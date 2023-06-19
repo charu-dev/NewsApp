1d70fe7c51408f615053a00bed4fa6847f446366
charu-dev <guptacharu105@gmail.com>
2023-06-19 13:32:56 +0530

    findone added in favourite. not working

--- app/api/favourite/new/route.js
+++ app/api/favourite/new/route.js
@@ -4,13 +4,24 @@ import { connectToDB } from "../../../../utils/database";
 export const POST = async (request) => {
     console.log("youareawesomecharuboss")
     const { feed } = await request.json();
-
+    const url=feed.url
+    // let hahaexist=false;
     try {
         await connectToDB();
-        const newfavFeed = new favFeed({ feed });
+        const feedexist= await favFeed.find( {"feed.url": url}).then((existingfeed)=>{console.log("han yr mil gaya");}).catch((err)=>{
+            console.log("sorry boss yeh to naya hai ");
+        });
+        if(feedexist){
+            console.log("yr yep ",feedexist.feed.url,url)
+            return 
+        }
+        else{
+            const newfavFeed = new favFeed({ feed });
+        
 
         await newfavFeed.save();
         return new Response(JSON.stringify(newfavFeed), { status: 201 })
+        }
     } catch (error) {
         console.log("yougetanerror",error)
         return new Response("Failed to create a new favFeed", { status: 500 });
